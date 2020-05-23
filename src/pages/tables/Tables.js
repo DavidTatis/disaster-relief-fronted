643fd471270c63502d4b396c6faa1393e3bcad18
Ariel Silva <ariel.silva@upr.edu>
2020-05-22 20:00:51 -0400

    Added reserve, request and buy functionality

--- src/pages/tables/Tables.js
+++ src/pages/tables/Tables.js
@@ -53,22 +53,48 @@ const columns = [
   options: {
    filter: true,
    sort: true,
+   
    customBodyRender: (value, tableMeta, updateValue) => {
+     var selectedQuantity;
             return (
-            <><input/>
-              <button onClick={() => {
-                console.log("value ", value);
+            <><input type="number" style={{width:50}} onChange={(event => selectedQuantity=event.target.value)}/>
+              <button onClick={ () => {
+                var d = new Date();
+                var m = "PayPal";
 
+                console.log("userID", localStorage.getItem("id_token"));
+                console.log("value", value);
+                console.log("quantity", selectedQuantity);
+                console.log("date", d.getTime())
+                console.log("method", m);
+
+                buyHandler(parseInt(localStorage.getItem("id_token")), parseInt(value) , parseInt(selectedQuantity), d.getTime(), m)
               }}>
                 Buy
               </button>
                 <button onClick={() => {
-                console.log("value ", value);
+                var d = new Date();
+
+                console.log("userID", localStorage.getItem("id_token"));
+                console.log("value", value);
+                console.log("quantity", selectedQuantity);
+                console.log("date", d.getTime())
+                
+
+                reserveHandler(parseInt(localStorage.getItem("id_token")), parseInt(value) , parseInt(selectedQuantity), d.getTime())
               }}>
                 Reserve
               </button>
                 <button onClick={() => {
-                console.log("value ", value);
+                var d = new Date();
+
+                console.log("userID", localStorage.getItem("id_token"));
+                console.log("value", value);
+                console.log("quantity", selectedQuantity);
+                console.log("date", d.getTime())
+                
+
+                requestHandler(parseInt(localStorage.getItem("id_token")), parseInt(value) , parseInt(selectedQuantity), d.getTime())
               }}>
                 Request
               </button>
@@ -79,6 +105,42 @@ const columns = [
  }
 
  ]
+
+async function buyHandler(userID, resourceID, quantity, date, method){
+  //send user id, resources id, quantities, date in milliseconds, payment method
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({uid: userID, rids : [resourceID], quantities : [quantity], date_milliseconds : date, method : method })
+  };
+  const response = await fetch('http://disaster-relief-x.herokuapp.com/aid/resources/buy/', requestOptions);
+  const data = await response.json();
+  return data;
+}
+
+async function reserveHandler(userID, resourceID, quantity, date){
+  //send user id, resources id, quantities, date in milliseconds
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({uid: userID, rids : [resourceID], quantities : [quantity], date_milliseconds : date })
+  };
+  const response = await fetch('http://disaster-relief-x.herokuapp.com/aid/resources/reserve/', requestOptions);
+  const data = await response.json();
+  return data;
+}
+
+async function requestHandler(userID, resourceID, quantity, date){
+  //send user id, resources id, quantities, date in milliseconds
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({uid: userID, rids : [resourceID], quantities : [quantity], date_milliseconds : date })
+  };
+  const response = await fetch('http://disaster-relief-x.herokuapp.com/aid/resources/request/', requestOptions);
+  const data = await response.json();
+  return data;
+}
 
 export class Tables extends React.Component {
     constructor(props) {
